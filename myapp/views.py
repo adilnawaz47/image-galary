7bf544974142aef1b12427cb94cd6663deba8695
adilnawaz47 <akhan8576800682@gmail.com>
2021-09-15 23:05:47 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from myapp.models import *
+from django.http import HttpResponseRedirect
+# Create your views here.
+def show_home_page(request):
+	cats=Category.objects.all()
+	images=Image.objects.all()
+	data={'images':images,'cats':cats}
+	return render(request,'home.html',data,)
+
+def show_category_page(request,cid):
+	cats=Category.objects.all()
+	category=Category.objects.get(pk=cid)
+	images=Image.objects.filter(cat=category)
+	data={'images':images,'cats':cats}
+	return render(request,'home.html',data,)
+
+def home(request):
+	return HttpResponseRedirect('/home')
